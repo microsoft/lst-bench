5664571dca2cdcd89ec979d61d3ddd7edb30860b
Ashvin Agrawal <ashvin@apache.org>
2023-06-28 20:05:06 -0700

    Refactor file name to match test naming convention

--- metrics/stability/__init__.py
+++ metrics/stability/__init__.py
@@ -1,5 +1,5 @@
 #stability/__init__.py
-# module for stability analysis
+"""module for stability analysis"""
 
 # Import public functions or classes
 from stability.degradation_analysis import DegradationEvaluatorBase
@@ -7,4 +7,3 @@ from stability.degradation_analysis import DegradationAnalysisHelper
 
 # Define __all__ to control what gets imported with wildcard import *
 __all__ = ['DegradationEvaluatorBase', 'DegradationAnalysisHelper']
-
