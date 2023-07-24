da27df276f0469b888058bbd6f1f063f0745250c
Anja Gruenheid <agruenheid@microsoft.com>
2023-07-24 17:15:46 +0200

    Refactoring executor chain.

---
+++ src/main/java/com/microsoft/lst_bench/common/SessionExecutor.java
@@ -0,0 +1,120 @@
+package com.microsoft.lst_bench.common;
+
+import com.microsoft.lst_bench.client.ClientException;
+import com.microsoft.lst_bench.client.Connection;
+import com.microsoft.lst_bench.client.ConnectionManager;
+import com.microsoft.lst_bench.exec.SessionExec;
+import com.microsoft.lst_bench.exec.TaskExec;
+import com.microsoft.lst_bench.telemetry.EventInfo;
+import com.microsoft.lst_bench.telemetry.EventInfo.EventType;
+import com.microsoft.lst_bench.telemetry.EventInfo.Status;
+import com.microsoft.lst_bench.telemetry.ImmutableEventInfo;
+import com.microsoft.lst_bench.telemetry.SQLTelemetryRegistry;
+import com.microsoft.lst_bench.util.DateTimeFormatter;
+import com.microsoft.lst_bench.util.StringUtils;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Default executor for sessions. Iterates over all tasks contained in the session
+ * and executes them sequentially.
+ */
+public class SessionExecutor implements Callable<Boolean> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SessionExecutor.class);
+
+    private final ConnectionManager connectionManager;
+    private final SQLTelemetryRegistry telemetryRegistry;
+    private final SessionExec session;
+    private final Map<String, Object> runtimeParameterValues;
+    private final Map<String, Instant> phaseIdToEndTime;
+    private String experimentStartTime;
+
+    public SessionExecutor(
+        ConnectionManager connectionManager,
+        SQLTelemetryRegistry telemetryRegistry,
+        SessionExec session,
+        Map<String, Object> runtimeParameterValues,
+        Map<String, Instant> phaseIdToEndTime,
+        String experimentStartTime) {
+        this.connectionManager = connectionManager;
+        this.telemetryRegistry = telemetryRegistry;
+        this.session = session;
+        this.runtimeParameterValues = runtimeParameterValues;
+        this.phaseIdToEndTime = phaseIdToEndTime;
+        this.experimentStartTime = experimentStartTime;
+    }
+
+    @Override
+    public Boolean call() throws ClientException {
+        Instant sessionStartTime = Instant.now();
+        try (Connection connection = connectionManager.createConnection()) {
+            for (TaskExec task : session.getTasks()) {
+                Map<String, Object> values = updateRuntimeParameterValues(task);
+                TaskExecutor taskExecutor = new TaskExecutor(connection, this.telemetryRegistry, task, values);
+                Instant taskStartTime = Instant.now();
+                try {
+                    taskExecutor.execute();
+                } catch (Exception e) {
+                    LOGGER.error("Exception executing task: " + task.getId());
+                    writeTaskEvent(taskStartTime, task.getId(), Status.FAILURE);
+                    throw e;
+                }
+                writeTaskEvent(taskStartTime, task.getId(), Status.SUCCESS);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Exception executing session: " + session.getId());
+            writeSessionEvent(sessionStartTime, session.getId(), Status.FAILURE);
+            throw e;
+        }
+        writeSessionEvent(sessionStartTime, session.getId(), Status.SUCCESS);
+        return true;
+    }
+
+    /**
+     * Update runtime parameters to contain time travel information if present in the task description.
+     * @param task
+     * @return Updated parameter mapping.
+     */
+    private Map<String, Object> updateRuntimeParameterValues(TaskExec task) {
+        Map<String, Object> values = new HashMap<>(this.runtimeParameterValues);
+        if (task.getTimeTravelPhaseId() != null) {
+            Instant ttPhaseEndTime = this.phaseIdToEndTime.get(task.getTimeTravelPhaseId());
+            if (ttPhaseEndTime == null) {
+                throw new RuntimeException(
+                    "Time travel phase identifier not found: " + task.getTimeTravelPhaseId());
+            }
+            // We round to the next second to make sure we are capturing the changes in case
+            // are consecutive phases
+            String timeTravelValue =
+                DateTimeFormatter.AS_OF_FORMATTER.format(
+                    ttPhaseEndTime.truncatedTo(ChronoUnit.SECONDS).plusSeconds(1));
+            values.put("asof", "TIMESTAMP AS OF " + StringUtils.quote(timeTravelValue));
+        } else {
+            values.put("asof", "");
+        }
+        return values;
+    }
+
+    private EventInfo writeSessionEvent(Instant startTime, String id, Status status) {
+        EventInfo eventInfo =
+            ImmutableEventInfo.of(
+                experimentStartTime, startTime, Instant.now(), id, EventType.EXEC_SESSION, status);
+        telemetryRegistry.writeEvent(eventInfo);
+        return eventInfo;
+    }
+
+    private EventInfo writeTaskEvent(Instant startTime, String id, Status status) {
+        EventInfo eventInfo =
+            ImmutableEventInfo.of(
+                experimentStartTime, startTime, Instant.now(), id, EventType.EXEC_TASK, status);
+        telemetryRegistry.writeEvent(eventInfo);
+        return eventInfo;
+    }
+
+}
