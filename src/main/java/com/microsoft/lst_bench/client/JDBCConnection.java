8638812a2ebf1dd25f3d43850d1f71d565c222b4
Anja Gruenheid <agruenheid@microsoft.com>
2023-07-31 18:07:31 +0200

    spotless

--- src/main/java/com/microsoft/lst_bench/client/JDBCConnection.java
+++ src/main/java/com/microsoft/lst_bench/client/JDBCConnection.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.sql.rowset.RowSetProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
