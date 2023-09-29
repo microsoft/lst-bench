d1e19dbcb28afe782a3ed64db64cd45000f2878a
Pooja Nilangekar <poojan@umd.edu>
2023-09-29 17:11:59 -0400

    Update DM phase for TPC-H

--- src/main/resources/scripts/tpch/data_maintenance/spark/RF1.sql
+++ src/main/resources/scripts/tpch/data_maintenance/spark/RF1.sql
@@ -5,11 +5,11 @@ INSERT
             orde_custkey,
             orde_orderstatus,
             orde_totalprice,
-            orde_orderdate,
             orde_orderpriority,
             orde_clerk,
             orde_shippriority,
-            orde_comment
+            orde_comment,
+            orde_orderdate
         FROM
             ${external_catalog}.${external_database}.s_orders_${stream_num};
 
@@ -26,11 +26,11 @@ INSERT
             liit_tax,
             liit_returnflag,
             liit_linestatus,
-            liit_shipdate,
             liit_commitdate,
             liit_receiptdate,
             liit_shipinstruct,
             liit_shipmode,
-            liit_comment
+            liit_comment,
+            liit_shipdate
         FROM
             ${external_catalog}.${external_database}.s_lineitem_${stream_num};
