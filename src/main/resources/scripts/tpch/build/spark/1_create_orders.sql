94ad4175d100a3b655bb99ff25d0ca3e5d082031
poojanilangekar <poojan@umd.edu>
2023-10-02 06:32:40 -0400

    Update column order in the TPC-H database  (#118)
    * Update column order in the TPC-H database
    
    * Update DM phase for TPC-H
    
    ---------
    
    Co-authored-by: Jesús Camacho Rodríguez <jesusca@microsoft.com>
    Co-authored-by: anjagruenheid <87397397+anjagruenheid@users.noreply.github.com>

--- src/main/resources/scripts/tpch/build/spark/1_create_orders.sql
+++ src/main/resources/scripts/tpch/build/spark/1_create_orders.sql
@@ -5,11 +5,11 @@ CREATE
             o_custkey BIGINT,
             o_orderstatus CHAR(1),
             o_totalprice DECIMAL,
-            o_orderdate DATE,
             o_orderpriority CHAR(15),
             o_clerk CHAR(15),
             o_shippriority INT,
-            o_comment VARCHAR(79)
+            o_comment VARCHAR(79),
+            o_orderdate DATE
         )
         USING ${table_format} OPTIONS(
             PATH '${data_path}${experiment_start_time}/${repetition}/orders/'
