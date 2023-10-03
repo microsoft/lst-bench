ef32ab23bd08a26da994ad3dd4345fdb0b07f68f
anjagruenheid <87397397+anjagruenheid@users.noreply.github.com>
2023-10-03 18:36:17 +0200

    Custom task executor implementation. (#144)

--- src/main/java/com/microsoft/lst_bench/task/TaskExecutor.java
+++ src/main/java/com/microsoft/lst_bench/task/TaskExecutor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.microsoft.lst_bench.common;
+package com.microsoft.lst_bench.task;
 
 import com.microsoft.lst_bench.client.ClientException;
 import com.microsoft.lst_bench.client.Connection;
