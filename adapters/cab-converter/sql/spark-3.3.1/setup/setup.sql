6aa56f971d642ce990ad974533ef575a788cae99
Jesus Camacho Rodriguez <jesusca@microsoft.com>
2024-09-29 11:31:15 -0700

    [CAB-adapter] Align SQL setup and build scripts for Spark with tpch-gen

--- adapters/cab-converter/sql/spark-3.3.1/setup/setup.sql
+++ adapters/cab-converter/sql/spark-3.3.1/setup/setup.sql
@@ -8,13 +8,13 @@ CREATE
     TABLE
         ${external_catalog}.${external_database}${stream_num}.customer(
             c_custkey BIGINT,
-            c_name VARCHAR(25),
-            c_address VARCHAR(40),
+            c_name STRING,
+            c_address STRING,
             c_nationkey BIGINT,
-            c_phone CHAR(15),
-            c_acctbal DECIMAL,
-            c_comment VARCHAR(117),
-            c_mktsegment CHAR(10)
+            c_phone STRING,
+            c_acctbal DECIMAL(12,2),
+            c_mktsegment STRING,
+            c_comment STRING
         )
         USING ${external_table_format} OPTIONS(
             PATH = "${external_data_path}${stream_num}/customer/" ${external_options_suffix}
@@ -29,19 +29,19 @@ CREATE
             l_orderkey BIGINT,
             l_partkey BIGINT,
             l_suppkey BIGINT,
-            l_linenumber INT,
-            l_quantity DECIMAL,
-            l_extendedprice DECIMAL,
-            l_discount DECIMAL,
-            l_tax DECIMAL,
-            l_returnflag CHAR(1),
-            l_linestatus CHAR(1),
+            l_linenumber BIGINT,
+            l_quantity DECIMAL(12,2),
+            l_extendedprice DECIMAL(12,2),
+            l_discount DECIMAL(12,2),
+            l_tax DECIMAL(12,2),
+            l_returnflag STRING,
+            l_linestatus STRING,
+            l_shipdate DATE,
             l_commitdate DATE,
             l_receiptdate DATE,
-            l_shipinstruct CHAR(25),
-            l_shipmode CHAR(10),
-            l_comment VARCHAR(44),
-            l_shipdate DATE
+            l_shipinstruct STRING,
+            l_shipmode STRING,
+            l_comment STRING
         )
         USING ${external_table_format} OPTIONS(
             PATH = "${external_data_path}${stream_num}/lineitem/" ${external_options_suffix}
@@ -55,13 +55,13 @@ CREATE
         ${external_catalog}.${external_database}${stream_num}.orders(
             o_orderkey BIGINT,
             o_custkey BIGINT,
-            o_orderstatus CHAR(1),
-            o_totalprice DECIMAL,
-            o_orderpriority CHAR(15),
-            o_clerk CHAR(15),
-            o_shippriority INT,
-            o_comment VARCHAR(79),
-            o_orderdate DATE
+            o_orderstatus STRING,
+            o_totalprice DECIMAL(12,2),
+            o_orderdate DATE,
+            o_orderpriority STRING,
+            o_clerk STRING,
+            o_shippriority BIGINT,
+            o_comment STRING
         )
         USING ${external_table_format} OPTIONS(
             PATH = "${external_data_path}${stream_num}/orders/" ${external_options_suffix}
@@ -74,9 +74,9 @@ CREATE
     TABLE
         ${external_catalog}.${external_database}${stream_num}.nation(
             n_nationkey BIGINT,
-            n_name CHAR(25),
+            n_name STRING,
             n_regionkey BIGINT,
-            n_comment VARCHAR(152)
+            n_comment STRING
         )
         USING ${external_table_format} OPTIONS(
             PATH = "${external_data_path}${stream_num}/nation/" ${external_options_suffix}
@@ -89,8 +89,8 @@ CREATE
     TABLE
         ${external_catalog}.${external_database}${stream_num}.region(
             r_regionkey BIGINT,
-            r_name CHAR(25),
-            r_comment VARCHAR(152)
+            r_name STRING,
+            r_comment STRING
         )
         USING ${external_table_format} OPTIONS(
             PATH = "${external_data_path}${stream_num}/region/" ${external_options_suffix}
@@ -103,14 +103,14 @@ CREATE
     TABLE
         ${external_catalog}.${external_database}${stream_num}.part(
             p_partkey BIGINT,
-            p_name VARCHAR(55),
-            p_mfgr CHAR(25),
-            p_type VARCHAR(25),
-            p_size INT,
-            p_container CHAR(10),
-            p_retailprice DECIMAL,
-            p_comment VARCHAR(23),
-            p_brand CHAR(10)
+            p_name STRING,
+            p_mfgr STRING,
+            p_brand STRING,
+            p_type STRING,
+            p_size BIGINT,
+            p_container STRING,
+            p_retailprice DECIMAL(12,2),
+            p_comment STRING
         )
         USING ${external_table_format} OPTIONS(
             PATH = "${external_data_path}${stream_num}/part/" ${external_options_suffix}
@@ -123,12 +123,12 @@ CREATE
     TABLE
         ${external_catalog}.${external_database}${stream_num}.supplier(
             s_suppkey BIGINT,
-            s_name CHAR(25),
-            s_address VARCHAR(40),
+            s_name STRING,
+            s_address STRING,
             s_nationkey BIGINT,
-            s_phone CHAR(15),
-            s_acctbal DECIMAL,
-            s_comment VARCHAR(101)
+            s_phone STRING,
+            s_acctbal DECIMAL(12,2),
+            s_comment STRING
         )
         USING ${external_table_format} OPTIONS(
             PATH = "${external_data_path}${stream_num}/supplier/" ${external_options_suffix}
@@ -142,9 +142,9 @@ CREATE
         ${external_catalog}.${external_database}${stream_num}.partsupp(
             ps_partkey BIGINT,
             ps_suppkey BIGINT,
-            ps_availqty INT,
-            ps_supplycost DECIMAL,
-            ps_comment VARCHAR(199)
+            ps_availqty BIGINT,
+            ps_supplycost DECIMAL(12,2),
+            ps_comment STRING
         )
         USING ${external_table_format} OPTIONS(
             PATH = "${external_data_path}${stream_num}/partsupp/" ${external_options_suffix}
